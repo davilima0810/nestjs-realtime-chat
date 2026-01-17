b7aa5ed63283e56f214a1e4c1781c4b4058ce752
Davi Lima <davicruz@seduc.pi.gov.br>
2026-01-16 23:09:08 -0300

    chore: reorganiza estrutura criando pasta backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('MONGO_URI');
+        if (!uri) {
+          throw new Error('MONGO_URI is not defined');
+        }
+        return { uri };
+      },
+    }),
+
+    UsersModule,
+
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
