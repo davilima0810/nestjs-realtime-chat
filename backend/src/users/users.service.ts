6902fd344d2974bf8d677a33e16e00d3a64b4395
Davi Lima <davicruz@seduc.pi.gov.br>
2026-01-16 23:10:03 -0300

    feat: adiciona módulo de usuários

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async create(data: Partial<User>): Promise<User> {
+    const user = new this.userModel(data);
+    return user.save();
+  }
+
+  async setOnline(userId: string, online: boolean): Promise<void> {
+    await this.userModel.updateOne({ _id: userId }, { online }).exec();
+  }
+}
