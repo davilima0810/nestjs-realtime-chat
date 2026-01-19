cf6909937fb117d017d5644ae98f17139480f486
Davi Lima <davicruz@seduc.pi.gov.br>
2026-01-18 23:32:23 -0300

    fix(backend): ajustes na inicialização da aplicação

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
